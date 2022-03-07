20a917195736623ee4968617f692521cf551feb6
Maple LearCemil <2419177287@qq.com>
2022-03-07 09:59:05 +0800

    Changes

---
+++ 504. 七进制数/Leetcode504.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 24191 on 2022/3/7.
+//
+#inlcude<iostream>
+using  namespace  std;
+
+
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        bool negative = num < 0;
+        num = abs(num);
+        string digits;
+        while (num > 0) {
+            digits.push_back(num % 7 + '0');
+            num /= 7;
+        }
+        if (negative) {
+            digits.push_back('-');
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
+
+
+
