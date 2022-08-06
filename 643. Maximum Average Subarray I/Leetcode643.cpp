a2a65039e0a15027ca62a009c8a0dd8c35097b02
Maple LearCemil <2419177287@qq.com>
2022-08-06 15:26:18 +0800

    commit

---
+++ 643. Maximum Average Subarray I/Leetcode643.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 24191 on 2022/8/6.
+//
+
+#include<iostream>
+#include<vector>
+#include<numeric>
+
+using namespace std;
+
+
+class Solution
+{
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        int sum = 0;
+        int n = nums.size();
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++)
+        {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = max(maxSum, sum);
+        }
+        return static_cast<double>(maxSum) / k;
+    }
+};
+
+int main()
+{
+    Solution s1;
+    vector<int> nums{0, 1, 1, 3, 3};
+    int k = 4;
+    cout << s1.findMaxAverage(nums, k) << endl;
+    return 0;
+}
