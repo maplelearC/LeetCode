6a4d6c4cf0a11b87e1d5814f8897fdc90c80f2ad
Maple LearCemil <2419177287@qq.com>
2022-09-19 15:43:29 +0800

    commit

---
+++ 15. 3Sum/Leetcode15.cpp
@@ -0,0 +1,110 @@
+//
+// Created by 24191 on 2022/9/19.
+//
+/*
+
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。注意：答案中不可以包含重复的三元组。
+
+示例 1：
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+解释：
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+注意，输出的顺序和三元组的顺序并不重要。
+
+示例 2：
+输入：nums = [0,1,1]
+输出：[]
+解释：唯一可能的三元组和不为 0 。
+
+示例 3：
+输入：nums = [0,0,0]
+输出：[[0,0,0]]
+解释：唯一可能的三元组和为 0 。
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        // a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0)
+            {
+                return result;
+            }
+
+            // 正确去重方法
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (right > left)
+            {
+                // 去重复逻辑如果放在这里，0，0，0 的情况，可能直接导致 right<=left 了，从而漏掉了 0,0,0 这种三元组
+                /*
+                while (right > left && nums[right] == nums[right - 1]) right--;
+                while (right > left && nums[left] == nums[left + 1]) left++;
+                */
+                if (nums[i] + nums[left] + nums[right] > 0)
+                {
+                    right--;
+                    // 当前元素不合适了，可以去重
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (nums[i] + nums[left] + nums[right] < 0)
+                {
+                    left++;
+                    // 不合适，去重
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                }
+                else
+                {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    // 去重逻辑应该放在找到一个三元组之后
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+
+                    // 找到答案时，双指针同时收缩
+                    right--;
+                    left++;
+                }
+            }
+
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s1;
+    vector<int> nums{-1, 0, 1, 2, -1, -4};
+    for (auto nums: s1.threeSum(nums))
+    {
+        for (auto num: nums)
+        {
+            cout << num << endl;
+        }
+    }
+    return 0;
+
+}
