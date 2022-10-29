a9df5989dd117c067407c52f6e9673d2d0674a28
Maple LearCemil <2419177287@qq.com>
2022-10-29 15:15:20 +0800

    commit

---
+++ 968. Binary Tree Cameras/Leetcode968.cpp
@@ -0,0 +1,75 @@
+//
+// Created by 24191 on 2022/10/29.
+//
+/*
+给定一个二叉树，我们在树的节点上安装摄像头。
+节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+计算监控树的所有节点所需的最小摄像头数量。
+
+
+示例 1：
+输入：[0,0,null,0,0]
+输出：1
+解释：如图所示，一台摄像头足以监控所有节点。
+
+示例 2：
+输入：[0,0,null,0,null,0,null,null,0]
+输出：2
+解释：需要至少两个摄像头来监视树的所有节点。 上图显示了摄像头放置的有效位置之一。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int res;
+
+    int traversal(TreeNode *root)
+    {
+        if (root == nullptr) return 2;
+        int left = traversal(root->left);
+        int right = traversal(root->right);
+
+        if (left == 2 && right == 2) return 0;
+        else if (left == 0 || right == 0)
+        {
+            res++;
+            return 1;
+        }
+        else return 2;
+    }
+
+    int minCameraCover(TreeNode *root)
+    {
+        res = 0;
+        if (traversal(root) == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+};
